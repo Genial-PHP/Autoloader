96b30a30e9532345f07708fb406a5fe89f1c047d
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-08 22:57:21 -0400

    Create the abstract autoloader.

---
+++ src/AbstractAutoloader.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+/**
+ * Elixir Framework
+ *
+ * @author  Nicholas English <https://github.com/Nenglish7>.
+ *
+ * @link    <https://github.com/elixir-framework/Autoloader> Github Repository.
+ * @license <https://github.com/elixir-framework/Autoloader/master/LICENSE> MIT License.
+ *
+ * @copyright Copyright (C) 2018 Elixir Framework
+ */
+namespace Elixir\Autoloader;
+/**
+ * AbstractAutoloader.
+ */
+class AbstractAutoloader
+{
+    /**
+     * Require a class based on path generation.
+     *
+     * @param string $path The path generated.
+     *
+     * @return void Return nothing.
+     */
+    protected function require(string $path): void {
+        require $path;
+    }
+}
